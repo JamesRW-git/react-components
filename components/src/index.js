67579ec1a32322c388bf5c269d2d938ff814e470
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-05 22:18:35 -0500

    setup comment text, time, and avatar through props

--- components/src/index.js
+++ components/src/index.js
@@ -7,9 +7,9 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
     return(
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:05PM" commentText="Hey look at this!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Today at 2:03PM" commentText="Second!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 8:00AM" commentText="Reaction!" avatar={faker.image.avatar()} />
         </div>
         );
 }
