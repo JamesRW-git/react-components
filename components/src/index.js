9fa9e88dd6a8850808fda0d1cc9869dab10dca2b
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-06 23:44:20 -0500

    add additional comment component

--- components/src/index.js
+++ components/src/index.js
@@ -10,6 +10,7 @@ const App = () => {
             <CommentDetail author="Sam" timeAgo="Today at 4:05PM" commentText="Hey look at this!" avatar={faker.image.avatar()} />
             <CommentDetail author="Alex" timeAgo="Today at 2:03PM" commentText="Second!" avatar={faker.image.avatar()} />
             <CommentDetail author="Jane" timeAgo="Yesterday at 8:00AM" commentText="Reaction!" avatar={faker.image.avatar()} />
+            <CommentDetail author="James" timeAgo="Yesterday at 7:42PM" commentText="Hmm...Reactive" avatar={faker.image.avatar()}/>
         </div>
         );
 }
