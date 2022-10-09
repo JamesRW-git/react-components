cd4b4b8dc2286a3c18c4bb97db90b4414eef8b41
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-08 22:41:23 -0500

    setup CommentDetail as child component of ApprovalCard

--- components/src/index.js
+++ components/src/index.js
@@ -8,31 +8,38 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:05PM"
-                commentText="Hey look at this!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:03PM"
-                commentText="Second!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 8:00AM"
-                commentText="Reaction!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="James"
-                timeAgo="Yesterday at 7:42PM"
-                commentText="Hmm...Reactive"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:05PM"
+                    commentText="Hey look at this!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:03PM"
+                    commentText="Second!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 8:00AM"
+                    commentText="Reaction!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="James"
+                    timeAgo="Yesterday at 7:42PM"
+                    commentText="Hmm...Reactive"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 }
