b9288f3dd5c1da0e9dda92ed52342a2a3b6f1d21
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-10 20:52:00 -0500

    add constructor method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div>Hi there!</div>
+class App extends React.Component {
+    //Not required for a React component
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null};
+    }
+
+    //React says we have to define render
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return <div>Latitude: </div>;
+    }
 }
 
 ReactDOM.render(
